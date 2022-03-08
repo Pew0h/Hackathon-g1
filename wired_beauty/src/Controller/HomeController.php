3544cedf5a7546c2eeaf3141c56246a50d741578
Pewoh <burl410@gmail.com>
2022-03-08 09:18:57 +0100

    Mise en place de la base index (home)

---
+++ wired_beauty/src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function viewHome(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+}
