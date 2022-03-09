57c113984ce29549a1e7a8a0c5f986c570b2bbf3
Pewoh <burl410@gmail.com>
2022-03-09 10:58:23 +0100

    Excel Parser optimisation + insertion BDD

--- wired_beauty/src/Controller/ExcelController.php
+++ wired_beauty/src/Controller/ExcelController.php
@@ -2,6 +2,11 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use App\Entity\Campain;
+use App\Entity\Choice;
+use App\Entity\Qcm;
+use App\Entity\Question;
+use Doctrine\ORM\EntityManagerInterface;
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,6 +15,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ExcelController extends AbstractController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+
     #[Route('/test', name: 'test')]
     public function viewTest(): Response
     {
@@ -30,11 +43,11 @@ class ExcelController extends AbstractController
 
     public function parseExcelToJson($file): Response
     {
-
         $reader = new Xlsx();
         $spreadsheet = $reader->load($file);
         $sheet = $spreadsheet->getActiveSheet();
-        $json = array();
+        $array = [];
+        $array_response = [];
         $letter = 'A';
         $questionNumber = 1;
 
@@ -42,24 +55,66 @@ class ExcelController extends AbstractController
             try{
                 $activeCell = $sheet->getCell($letter. $i)->getValue();
             }catch(\Exception $e){
-                break; // Nous avons atteint la fin de l'excel
+                break; // We have reached the end of the excel
             }
             if($activeCell){
-                if((ord(strtoupper($letter)) - ord('A') + 1) % 2){
-                    $json['question_'. $questionNumber] = $activeCell;
+                if((ord(strtoupper($letter)) - ord('A') + 1) % 2){ // Check if the letter of the column is odd
+                    $array[$questionNumber]['question'] = $activeCell;
                 }
-                else{
-                    $json['response_'. $questionNumber . '_' . $i-1] = $activeCell;
+                else{ // We add the answers in the table
+                    $array_response[] = $activeCell;
                 }
 
             }else{
-                $i = 1; // On recommence en début de la première ligne de la colonne
-                ++$letter; // On passe à la prochaine colonne
+
+                $i = 1; // Start again at the beginning of the first row of the column
+                ++$letter; // We go to the next column (Next letter)
                 if((ord(strtoupper($letter)) - ord('A') + 1) % 2){
+                    if($array_response){
+                        $array[$questionNumber]['response'] = $array_response;
+                        $array_response = [];
+                    }
                     $questionNumber++;
                 }
-                // On incrémente le numéro de la question
             }
+        }
+        $this->addQCMToDatabase($array);
+    }
+
+    public function addQCMToDatabase(array $array){
+        // Get first campain for example
+        $campain = $this->em->getRepository(Campain::class)->find(1);
+
+        // Create QCM
+        $qcm = new Qcm();
+        $qcm->setName('QCM TEST 2');
+        $qcm->setCampain($campain);
+        $this->em->persist($qcm);
+
+        // Get the total number of questions
+        $questions_numbers = 0;
+        foreach ($array as $key => $value){
+            if (array_key_exists('question', $value)){
+                $questions_numbers++;
+            }
+        }
+
+        //Creation of the QCM with the associated questions and answers
+        for ($i = 1; $i < $questions_numbers; $i++){
+            // Question creation
+            $questions = new Question();
+            $questions->setQcm($qcm);
+            $questions->setName($array[$i]['question']);
+            $this->em->persist($questions);
+            $this->em->flush();
+
+            foreach ($array[$i]['response'] as $value) {
+                $choice = new Choice();
+                $choice->setQuestion($questions);
+                $choice->setValue($value);
+                $this->em->persist($choice);
+            }
+            $this->em->flush();
         }
     }
 
