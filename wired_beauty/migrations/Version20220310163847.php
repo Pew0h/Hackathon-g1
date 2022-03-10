bd45b0e4be2b4930ada6333339ffd6ab731b31e3
Delphine <delphinelcrx@gmail.com>
2022-03-10 17:42:36 +0100

    fix fixtures campain registration

--- wired_beauty/migrations/Version20220310163847.php
+++ wired_beauty/migrations/Version20220310163847.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220310080349 extends AbstractMigration
+final class Version20220310163847 extends AbstractMigration
 {
     public function getDescription(): string
     {
